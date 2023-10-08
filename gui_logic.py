f9960d65d169675f87dfb97448594deeb7832c3b
SkorEgor <92719975+SkorEgor@users.noreply.github.com>
2023-10-08 11:21:54 +0300

    Drawing_and_scaling

---
+++ gui_logic.py
@@ -0,0 +1,23 @@
+from gui import Ui_Dialog
+from canvas import Canvas
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtCore import Qt
+
+# КЛАСС АЛГОРИТМА ПРИЛОЖЕНИЯ
+class GuiProgram(Ui_Dialog):
+
+    def __init__(self, dialog):
+        # ПОЛЯ КЛАССА
+
+
+        # ДЕЙСТВИЯ ПРИ ВКЛЮЧЕНИИ
+        # Создаем окно
+        Ui_Dialog.__init__(self)
+        self.setupUi(dialog)  # Устанавливаем пользовательский интерфейс
+
+        self.canvas = Canvas()
+        self.plotLayout.addWidget(self.canvas)
+
+
+        self.last_x, self.last_y = None, None
